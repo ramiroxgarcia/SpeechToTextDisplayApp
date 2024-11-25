04edb391062b3ef8c73dff74b388ac0abd7d5e7e
ramiroxgarcia <ramirogarcia3001@gmail.com>
2024-11-25 16:00:02 -0500

    working with ui (done)

--- SpeechToTextGlassesApp/Voice.tsx
+++ SpeechToTextGlassesApp/Voice.tsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, Button, PermissionsAndroid, Platform, Alert } from 'react-native';
+import { View, Text, Button, PermissionsAndroid, Platform, Alert, StyleSheet, Pressable } from 'react-native';
 import Voice, { SpeechResultsEvent, SpeechErrorEvent } from '@react-native-voice/voice';
 import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions'; // Import permissions
 import BleManager from 'react-native-ble-manager'
+import {LogBox} from 'react-native';
 import { BleManager as BlePlxManager, Device, NativeDevice } from 'react-native-ble-plx';
+
 
 global.Buffer = require('buffer').Buffer;
 const blePlxManager = new BlePlxManager()
-
+LogBox.ignoreLogs([
+  /`new NativeEvent.*/,
+]);
 
 const VoiceRecognition = () => {
   const [recognizedText, setRecognizedText] = useState<string>('');
   const [error, setError] = useState<string>('');
+  const [connectError, setConnectionError] = useState<string>('');
   const [isListening, setIsListening] = useState<boolean>(false); // Track listening state
-
+  const [listeningState, setlisteningState] = useState<boolean>(false); 
   const [espConnected, setEspConnected] = useState(false);
-
+  const [isPressed, setIsPressed] = useState(false);
   const espConnectedRef = useRef(espConnected)
 
+  const listeningStateRef = useRef(listeningState)
   const espServiceUUID = '';
   const espCharacteristicUUID = '';
 
@@ -36,7 +42,7 @@ const VoiceRecognition = () => {
     // const dev = bleManager.cancelDeviceConnection('30:C9:22:B1:3F:7E' as DeviceId ?? '' as DeviceId)
     // console.log('device closed: ' + dev)
 
-    startBluetooth();
+    // startBluetooth();
 
 
 
@@ -58,6 +64,9 @@ const VoiceRecognition = () => {
       });
     };
   }, []);
+  useEffect(() => {
+    listeningStateRef.current = listeningState;
+  }, [listeningState]);
 
   useEffect(() => {
     espConnectedRef.current = espConnected;
@@ -143,14 +152,18 @@ const VoiceRecognition = () => {
         // Success code
         console.log("Connected");
         setEspConnected(true);
+        setConnectionError('');
       })
       .catch((error) => {
         // Failure code
         console.log(error);
+        setConnectionError(error);
+
       });
   }
 
   const sendResultString = async (sendString: string) => {
+
     // console.log(deviceRef.current)
     if (!espConnectedRef.current) {
       console.log(espConnectedRef.current)
@@ -186,7 +199,7 @@ const VoiceRecognition = () => {
         try {
           let charc = await BleManager.read('30:C9:22:B1:3F:7E', 'ABCD', '2222')
           console.log('pure read ' + charc)
-          test = charc[0].toString()
+          test = String.fromCharCode(charc[0])
           // const charc = await device.readCharacteristicForService("ABCD", "2222")
           // test = charc?.value ? Buffer.from(charc.value, 'base64').toString('utf-8') : ""
         }
@@ -201,9 +214,18 @@ const VoiceRecognition = () => {
       console.log('test just read: ' + test)
 
       try {
-        console.log(sendString)
+        console.log(sendString.length)
+
+        if (sendString.length > 255) {
+          console.log('hit len')
+          sendString = sendString.substring(0, 255)
+          console.log(sendString.length)
+        }
         
-        const buffer = Buffer.from(sendString)
+        const buffer = Buffer.from(sendString + "\0")
+        
+
+
         console.log('sending: ' + buffer.toJSON().data)
 
 
@@ -211,12 +233,13 @@ const VoiceRecognition = () => {
         const messageString = String.fromCharCode(...buffer.toJSON().data);
 
         // encode the string to base64
-        const base64Message = Buffer.from(messageString).toString('base64');
+        // const base64Message = Buffer.from(messageString).toString('base64');
 
-        const base64AsciiArray = Array.from(base64Message).map(char => char.charCodeAt(0));
+        // let messageEncoded = Array.from(messageString).map(char => char.charCodeAt(0));
         
-        // await BleManager.write('30:C9:22:B1:3F:7E', 'ABCD', '2222', base64AsciiArray)
+        await BleManager.write('30:C9:22:B1:3F:7E', 'ABCD', '1111', buffer.toJSON().data, 255)
 
+        // console.log('just sent: ' + messageEncoded)
         // console.log('sending: ' + Buffer.from(sendString, 'utf-8').toString('base64'))
         // await device.writeCharacteristicWithResponseForService(
         //   "ABCD",
@@ -232,7 +255,7 @@ const VoiceRecognition = () => {
 
       const buffer = Buffer.from(sendString)
 
-      await BleManager.write('30:C9:22:B1:3F:7E', 'ABCD', '2222', [0])
+      await BleManager.write('30:C9:22:B1:3F:7E', 'ABCD', '2222', [48])
       // await device.writeCharacteristicWithoutResponseForService(
       //   "ABCD",
       //   "2222",
@@ -257,7 +280,11 @@ const VoiceRecognition = () => {
 
       await sendResultString(event.value[0]); // send recognized text esp
 
-      startListening();
+      console.log(listeningStateRef.current)
+      if(listeningStateRef.current){
+        console.log('still istening')
+        startListening();
+      }
     }
   };
 
@@ -267,15 +294,19 @@ const VoiceRecognition = () => {
     } else {
       setError('Unknown error occurred');
     }
-    startListening();
+    if(listeningStateRef.current){
+      startListening();
+    }
   };
 
   // Request Microphone Permission
   const requestMicrophonePermission = async () => {
     const result = await request(PERMISSIONS.ANDROID.RECORD_AUDIO);
+    setlisteningState(true);
     if (result === RESULTS.GRANTED) {
       console.log("Microphone permission granted");
       startListening(); // Start listening after permission is granted
+      setlisteningState(true);
     } else {
       Alert.alert("Permission Denied", "Microphone access is required to use voice recognition.");
     }
@@ -289,41 +320,101 @@ const VoiceRecognition = () => {
       setIsListening(true); // Set listening state to true
       setError('');
     } catch (e) {
-      console.error(e);
+      // console.error(e);
     }
     // await sendResultString("Under the clear night sky, the city lights flickered like tiny stars, casting a warm glow over the quiet streets. A gentle wind whispered through the trees,")
   };
 
   const stopListening = async () => {
     try {
+      console.log('hit1')
       await Voice.stop();
+      setlisteningState(false);
       setIsListening(false); // Set listening state to false
+      console.log(isListening)
+      setRecognizedText('');
     } catch (e) {
-      console.error(e);
+      console.log('hit stop')
+      // console.error(e);
     }
   };
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    button33: {
+      borderRadius: 100,
+      shadowColor: '#2CBB63',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      shadowRadius: 10,
+      color: 'green',
+      paddingVertical: 7,
+      paddingHorizontal: 20,
+      textAlign: 'center',
+      fontSize: 16,
+      fontFamily: 'CerebriSans-Regular',
+      borderWidth: 0,
+      transform: [{ scale: 1 }],
+      width: 190
+        
+      ,
+      alignContent: 'center',
+      margin: 10,
+      elevation: 20, // Android shadow
+      
+    },
+    button33Pressed: {
+      backgroundColor: '#a9d6bc', // Darker shade of the button color
+      transform: [{ scale: 0.95 }], // Scale down for "push in" effect
+    },
+    text: {
+      color: 'black',
+      textAlign: 'center',
+      fontSize: 16,
+      fontWeight : 'bold',
+      fontFamily: 'monospace'
+    }
+  });
+
   return (
-    <View>
+    <View style={styles.container}>
       {espConnected ? (
         <>
-          <Button 
-            title={isListening ? "Listening..." : "Start Listening"} 
-            onPress={isListening ? stopListening : requestMicrophonePermission} 
-          />
-          {isListening && (
-            <Button title="Stop Listening" onPress={stopListening} />
-          )}
-          <Text style={{ fontSize: 24, padding: 20 }}>Recognized Text: {recognizedText}</Text>
-          {error ? <Text>Error: {error}</Text> : null}
+          <Pressable
+          style={[styles.button33,
+            isPressed && styles.button33Pressed, ]}
+          onPressIn={() => setIsPressed(true)} // Button is pressed
+          onPressOut={() => setIsPressed(false)} // Button is released\
+          onPress={isListening ? stopListening : requestMicrophonePermission}>
+            <Text style={styles.text}>
+              {isListening ? "Stop Listening" : "Start Listening"}
+            </Text>
+          </Pressable>
+          <Text style={{ fontSize: 24, padding: 20, fontFamily: 'monospace' }}>{recognizedText}</Text>
         </>
       ) : (
-        <View>
-          <Text style={{margin: "auto"}}>Attachment not connected</Text>
+        <View style={styles.container}>
+          <Pressable style={[styles.button33, isPressed && styles.button33Pressed]} 
+          onPress={startBluetooth}
+          onPressIn={() => setIsPressed(true)} // Button is pressed
+          onPressOut={() => setIsPressed(false)} // Button is released\
+          >
+            <Text style={styles.text}>
+              Connect ᛒ
+            </Text>
+          </Pressable>
+          <Text style={{ fontSize: 24, padding: 20, fontFamily: 'monospace' }}>Attachment not connected</Text>
+          {connectError.length > 1 && <Text style={{ fontSize: 24, padding: 20, fontFamily: 'monospace' }}>{connectError}. Try again.</Text>
+        }
         </View>
       )}
     </View>
   );
+  
 };
 
 export default VoiceRecognition;
